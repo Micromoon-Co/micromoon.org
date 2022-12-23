12f897c9532e99251a0e5a73fbd569ad4454a434
Mehmet Can Kahveci <96494651+mehmetcankahveci@users.noreply.github.com>
2022-12-23 21:13:07 +0300

    Add files via upload

---
+++ meme.js
@@ -0,0 +1,84 @@
+let meme
+let nsfwToggle = document.getElementById("nsfw-toggle")
+let timerDisplay = document.getElementById("timer")
+let seconds
+
+function requestMeme() {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "https://meme-api.com/gimme", false);
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.responseText);
+}
+
+function setNSFWToggle() {
+    if (getNSFWCookie()) {
+        nsfwToggle.textContent = "NSFW Gösterme"
+    } else {
+        nsfwToggle.textContent = "NSFW Göster"
+    }
+
+}
+
+function getNSFWCookie() {
+    try {
+        return document.cookie.split("=")[1].split(";")[0] === "true"
+    }
+    catch (TypeError) {
+        setNSFWCookie(false)
+        return false
+    }
+}
+
+function setNSFWCookie(value) {
+    document.cookie = "nsfw=" + value + "; Sat, 13 Sep 275760 00:00:00 GMT"
+}
+
+function toggleNSFW() {
+    setNSFWCookie(!getNSFWCookie())
+    setNSFWToggle()
+}
+
+
+function getMeme() {
+    seconds = 30
+    timerDisplay.textContent = "Yenileniyor..."
+    meme = requestMeme()
+    if (!getNSFWCookie()) {
+        while (meme.nsfw === true) {
+            meme = requestMeme()
+        }
+    }
+    document.getElementById("meme").src = meme.url
+    document.getElementById("title").innerHTML = "Başlık: " + meme.title
+    document.getElementById("title-url").href = meme.postLink
+    document.getElementById("download").download = meme.url
+    
+    if (meme.nsfw) {
+        document.getElementById("title").innerHTML += " (NSFW)"
+    }
+    document.getElementById("source-sub").innerHTML = "Kaynak Subreddit: " + meme.subreddit
+    document.getElementById("source-sub-url").href = "https://reddit.com/r/" + meme.subreddit
+}
+
+function timer() {
+    if (seconds === 0) {
+        getMeme()
+    }
+    seconds--
+    timerDisplay.textContent = seconds + " saniyeye yenilenecek"
+}
+
+function closePopup() {
+    document.getElementById("popup").style = "display: none"
+}
+function openPopup() {
+    document.getElementById("popup").style = "display: flex"
+}
+getMeme()
+document.getElementById("meme").addEventListener("click", getMeme)
+nsfwToggle.addEventListener("click", toggleNSFW)
+document.getElementById("close").addEventListener("click", closePopup)
+document.getElementById("download").addEventListener("click", openPopup)
+setNSFWToggle()
+
+setInterval(timer, 1000)
